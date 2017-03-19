07b9068c839df60e12f3e0a4bb724a33872bad47
Abdeali Bhagat <abdealibhagat787@gmail.com>
2017-03-19 18:21:34 +0530

    Adding comments

--- File-Handling.c
+++ File-Handling.c
@@ -2,38 +2,46 @@
 #include<conio.h>
 #include<string.h>
 
+//Creating Empl structure
 struct Empl{
+    //Adding data types to store name and id's of the Employee's
     char name[100];
     int empl_id;
 };
+
 void main(){
-    struct Empl e[10];
-    FILE *file_append, *file_read;
+
+    struct Empl e[10]; // Defining array of structure variables
+    FILE *file_append, *file_read; 
     char buff[1000];
     int i;
 
-    file_append = fopen("employee-details.txt", "a+");
-    file_read = fopen("employee-details.txt", "r");
-
+    file_append = fopen("employee-details.txt", "a+"); // fileobject to append employee details into employee-details.txt
+    file_read = fopen("employee-details.txt", "r"); // fileobject to read the created employee-details.txt
+    
     /* Getting Input from User and Appending to the file employee-details. */
     if (file_append != NULL) {
+        // Asking users to enter 10 employee name's and id's
+
         for(i = 0; i < 10; i++){
             printf("Enter Name of Employee and Employee ID of the Employee:\n");
             scanf("%s%d", &e[i].name, &e[i].empl_id);
+            // Printing it to the employee-details.txt file
             fprintf(file_append, "Employee %s has Employee ID %d.\n", e[i].name, e[i].empl_id);
         }
         printf("File Created.\n");
-        fclose(file_append);
+        fclose(file_append); // Closing the file
     }
     else {
         printf("The file wasn't Created.\n");
     }
 
-    /* Reading from the employee-details And Displaying It. */
+    /* Reading from the employee-details.txt line by line  And Displaying It. */
     printf("Contents of file are: \n");
     while (fgets(buff, 1000, file_read) !=NULL) {
        printf("%s", buff);
    }
-   fclose(file_append);
+   fclose(file_read); // Closing the file
+    
    getch();
 }
